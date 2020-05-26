54c631d8b7a706fc788ba9cb77dcdf174a0f7723
edlearningmx <edlearningmx@gmail.com>
2020-05-25 23:21:24 -0500

    created new repository for tworld

---
+++ E2ESeleniumJenkintsTWorld/src/testCases/TC01.java
@@ -0,0 +1,21 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC01 {
+	@Test
+	public void test01()
+	{
+		System.setProperty("webdriver.chrome.driver", "Drivers/80chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("hello");
+		driver.findElement(By.id("pass")).sendKeys("hello");
+		driver.quit();
+		
+		
+	}
+}
